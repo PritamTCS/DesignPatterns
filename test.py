850d8d86b5eb591d4134c8f5620671c6b38ef4ef
Pritam <pritam123@gmail.com>
2019-06-21 09:55:44 +0530

    second commit

--- test.py
+++ test.py
@@ -1,6 +1,24 @@
-entries = []
-count = 0
-text = "Hello World"
+# entries = []
+# count = 0
+# text = "Hello World"
 
-entries.append(f'{count}:{text}')
-print(entries)
+# entries.append(f'{count}:{text}')
+# print(entries)
+
+from enum import Enum
+
+
+class Color(Enum):
+    RED = 1
+    GREEN = 2
+    BLUE = 3
+
+
+class Size(Enum):
+    SMALL = 1
+    MEDIUM = 2
+    LARGE = 3
+
+
+for color in Color:
+    print(color.value)
